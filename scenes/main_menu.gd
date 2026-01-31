00ba14681d07c8a5ade7ac1b282e3049b2cfb776
carlosaltan18 <carlosaltan850@gmail.com>
2026-01-31 13:21:45 -0600

    Menú principal del juego

---
+++ scenes/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+# Referencias a los botones
+@onready var new_game_button = $MenuContainer/SpacerTop/ButtonsContainer/Start
+@onready var exit_button = $MenuContainer/SpacerTop/ButtonsContainer/Exit
+@onready var credits_button = $MenuContainer/SpacerTop/ButtonsContainer/Credits
+
+func _ready():
+	# Conectar las señales de los botones
+	new_game_button.pressed.connect(_on_new_game_pressed)
+	exit_button.pressed.connect(_on_quit_pressed)
+	credits_button.pressed.connect(_on_credits_pressed)
+	
+func _on_new_game_pressed():
+	print("Iniciando nuevo juego...")
+	# Aquí cambiarás a la escena del juego
+
+func _on_credits_pressed():
+	print("Abriendo Creditos...")
+	# Aquí abrirás el menú de configuración
+
+func _on_quit_pressed():
+	print("Saliendo del juego...")
+	get_tree().quit()
